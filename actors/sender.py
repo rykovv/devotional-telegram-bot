734b60ee0262ab02a49585e7e12dcbadba833644
Vladislav Rykov <vladoss@list.ru>
2021-07-12 00:55:07 -0700

    message composition and audio sending

--- actors/sender.py
+++ actors/sender.py
@@ -2,10 +2,12 @@ from configparser import ConfigParser
 
 import telegram
 from db.base import Session
-from utils.utils import get_current_utc_hour
+from utils.utils import get_current_utc_hour, get_today_month_day
 import utils.consts as consts
 
 from db.subscription import Subscription
+
+import actors.composer as composer
 
 config = ConfigParser()
 config.read(consts.CONFIG_FILE_NAME)
@@ -16,9 +18,11 @@ def send():
     subscriptions = session.query(Subscription).filter(Subscription.preferred_time_utc == current_hour).all()
 
     bot = telegram.Bot(token=config['bot']['token'])
-    msg = 'That\'s today\'s devotional!'
+    now = get_today_month_day()
 
     for subscription in subscriptions:
-        bot.sendMessage(chat_id=str(subscription.subscriber_id), text=msg, parse_mode=telegram.ParseMode.HTML)
+        msg, title = composer.compose(subscription.devotional_name, now[0], now[1])
+        bot.send_message(chat_id=str(subscription.subscriber_id), text=msg, parse_mode='html')
+        bot.send_audio(chat_id=str(subscription.subscriber_id), audio=open(f'files/mp3/maranata/{now[0]}/{now[1]}.mp3', 'rb'), title=title, timeout=300)
 
     print(f'devotionals sent at {consts.TF_24TO12[get_current_utc_hour()]}')
