cc2cc628f276bd41175f8e22d91d40f0fc62eb8b
Vladislav Rykov <vladoss@list.ru>
2021-07-26 18:51:25 -0700

    absent devotional days processing and special message

--- actors/composer.py
+++ actors/composer.py
@@ -3,11 +3,22 @@ from db.base import Session
 
 def compose(name, month, day):
     session = Session()
-    devotional = session.query(Devotional).filter(Devotional.name == name, Devotional.month == str(month), Devotional.day == str(day)).all()[0]
-    message = f'\U0001F4C5<b> {devotional.date}</b>\n\U0001F4C3 <b>{devotional.title}</b>\n\n <i>\U0001F4D6 {devotional.verse}</i>\n\n'
-    for k, v in devotional.paragraphs.items():
-        message += (v + '\n\n')
-    message += f'{devotional.url}'
+    devotional = session.query(Devotional).filter(Devotional.name == name, Devotional.month == str(month), Devotional.day == str(day)).all()
     session.close()
+    audio_id = ''
+    if devotional != []:
+        devotional = devotional[0]
+        message = f'\U0001F4C5<b> {devotional.date}</b>\n\U0001F4C3 <b>{devotional.title}</b>\n\n <i>\U0001F4D6 {devotional.verse}</i>\n\n'
+        for k, v in devotional.paragraphs.items():
+            message += (v + '\n\n')
+        message += f'{devotional.url}'
+        audio_id = devotional.audio_file_ids
+    else:
+        message =   'Querido hermano/hermana,\n\n' \
+                    'Lamentablemente, esta matutina originalmente no tiene este día. ' \
+                    'Mañana Usted va a recibir el devocional del día. ' \
+                    'Esto ocurre porque algunos devocionales no tienen días como el 29 de Febrero.\n\n' \
+                    'Pedimos nuestras disculpas,\n' \
+                    'El equipo de Una Mirada de Fe y Esperanza'
 
-    return (message, devotional.audio_file_ids)
+    return (message, audio_id)
