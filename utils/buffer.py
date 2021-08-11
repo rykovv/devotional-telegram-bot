dde461a3b52c8ad4780ab20e06ae6b056f21dfc4
Vladislav Rykov <vladoss@list.ru>
2021-08-10 23:20:00 -0700

    buffer modified to handle quizzes

--- utils/buffer.py
+++ utils/buffer.py
@@ -1,23 +1,35 @@
+from db.subscriber import Subscriber
+from db.subscription import Subscription
+from db.quiz import Quiz
 
-subscribers = {}
-subscriptions = {}
+subscribers: dict[int, Subscriber] = {}
+subscriptions: dict[int, Subscription] = {}
+quizzes: dict[int, Quiz] = {}
 
-
-def add_subscriber(subscriber):
+def add_subscriber(subscriber: Subscriber):
     subscribers[subscriber.id] = subscriber
 
-def delete_subscriber(subscriber_id):
+def delete_subscriber(subscriber_id: int):
     if subscriber_id in subscribers:
         subscribers.pop(subscriber_id, None)
 
-def add_subscription(subscription):
+def add_subscription(subscription: Subscription):
     subscriptions[subscription.subscriber_id] = subscription
 
 # subscriber can modify only one subscription at a time
-def delete_subscription(subscriber_id):
+def delete_subscription(subscriber_id: int):
     if subscriber_id in subscriptions:
         subscriptions.pop(subscriber_id, None)
 
-def clean(subscriber_id):
+# subscriber can take only one quiz at a time
+def add_quiz(subscriber_id: int, quiz: Quiz):
+    quizzes[subscriber_id] = quiz
+
+def delete_quiz(subscriber_id: int):
+    if subscriber_id in quizzes:
+        quizzes.pop(subscriber_id, None)
+
+def clean(subscriber_id: int):
     delete_subscriber(subscriber_id)
     delete_subscription(subscriber_id)
+    delete_quiz(subscriber_id)
