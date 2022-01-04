00acc110e6380023f8a0695c31f6a611952decb8
Vladislav Rykov <vladoss@list.ru>
2022-01-04 00:07:17 -0800

    parse generalized and moved to utils as parse_bible_reference

--- db/promise.py
+++ db/promise.py
@@ -26,16 +26,3 @@ class Promise(Base):
         self.random_order = random_order
         self.verse_bible_reference = verse_bible_reference
         self.optional = optional
-
-    # verses = [[1, 4],[10]]
-    def parse(self):
-        s = self.verse_bible_reference.split(':')
-        book = s[0].split(' ')[:-1].join(' ')
-        chapter = int(s[0].split(' ')[-1])
-        verses = [] 
-        for v in s[1].split(','):
-            if v.find('-') >= 0:
-                verses.append([int(v.split('-')[0]), int(v.split('-')[1])])
-            else:
-                verses.append([int(v)])
-        return book, chapter, verses
